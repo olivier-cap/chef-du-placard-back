3db3ca621eb7f89defe4b4e829c85b3a5f3a2f3e
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-05-26 17:04:08 +0200

    UnitRepository return Optionals instead of values

--- src/main/java/io/github/oliviercap/chefduplacard/application/ports/persistence/IUnitRepository.java
+++ src/main/java/io/github/oliviercap/chefduplacard/application/ports/persistence/IUnitRepository.java
@@ -4,10 +4,11 @@ import io.github.oliviercap.chefduplacard.adapters.persistence.jpa.JPAentity.Uni
 import io.github.oliviercap.chefduplacard.domain.unit.Unit;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUnitRepository {
-    Unit findUnitByName(String name);
-    Unit findUnitById(Long id);
+    Optional<Unit> findUnitByName(String name);
+    Optional<Unit> findUnitById(Long id);
 
     List<UnitJpa> findAllJpa();
 }
