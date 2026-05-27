c0b83058628a2cbc19901391e465dd0b431295e5
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-05-27 19:29:58 +0200

    GetAliments Presenter, ResponseModel and ViewModel

---
+++ src/main/java/io/github/oliviercap/chefduplacard/adapters/web/getaliments/presenters/GetAlimentsPresenter.java
@@ -0,0 +1,32 @@
+package io.github.oliviercap.chefduplacard.adapters.web.getaliments.presenters;
+
+import io.github.oliviercap.chefduplacard.adapters.web.getaliments.GetAlimentsViewModel;
+import io.github.oliviercap.chefduplacard.application.dto.AlimentResponse;
+import io.github.oliviercap.chefduplacard.application.getaliments.GetAlimentsResponseModel;
+import io.github.oliviercap.chefduplacard.application.getaliments.ports.IGetAlimentsOutputPort;
+
+public class GetAlimentsPresenter implements IGetAlimentsOutputPort {
+    private GetAlimentsViewModel viewModel;
+
+    @Override
+    public void displayAliments(GetAlimentsResponseModel responseModel) {
+        viewModel = new GetAlimentsViewModel(
+                responseModel.alimentResponses().stream()
+                        .map(this::toAlimentViewModel)
+                        .toList()
+        );
+    }
+
+    @Override
+    public GetAlimentsViewModel getViewModel() {
+        return viewModel;
+    }
+
+    private GetAlimentsViewModel.AlimentViewModel toAlimentViewModel(AlimentResponse alimentResponse) {
+        return new GetAlimentsViewModel.AlimentViewModel(
+                alimentResponse.name(),
+                alimentResponse.description(),
+                alimentResponse.active()
+        );
+    }
+}
