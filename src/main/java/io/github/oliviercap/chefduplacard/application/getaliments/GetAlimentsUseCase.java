b8be0737d52ae52e401486900ff7e5cfd276ee91
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-05-27 19:20:20 +0200

    Get Aliment usecase class, RequestModel

---
+++ src/main/java/io/github/oliviercap/chefduplacard/application/getaliments/GetAlimentsUseCase.java
@@ -0,0 +1,44 @@
+package io.github.oliviercap.chefduplacard.application.getaliments;
+
+import io.github.oliviercap.chefduplacard.adapters.persistence.mapper.aliment.AlimentMapper;
+import io.github.oliviercap.chefduplacard.application.dto.AlimentResponse;
+import io.github.oliviercap.chefduplacard.application.getaliments.ports.IGetAlimentsInputPort;
+import io.github.oliviercap.chefduplacard.application.getaliments.ports.IGetAlimentsOutputPort;
+import io.github.oliviercap.chefduplacard.application.ports.persistence.IAlimentRepository;
+import io.github.oliviercap.chefduplacard.domain.food.Aliment;
+
+import java.util.List;
+
+/**
+ * UseCase to view of aliments existing in the database
+ */
+public class GetAlimentsUseCase implements IGetAlimentsInputPort {
+    private final IAlimentRepository alimentRepository;
+    private final AlimentMapper alimentMapper;
+    private final IGetAlimentsOutputPort outputPort;
+
+    public GetAlimentsUseCase(IAlimentRepository alimentRepository,
+                              AlimentMapper alimentMapper,
+                              IGetAlimentsOutputPort outputPort) {
+        this.alimentRepository = alimentRepository;
+        this.alimentMapper = alimentMapper;
+        this.outputPort = outputPort;
+    }
+
+    @Override
+    public void execute(GetAlimentsRequestModel requestModel) {
+        List<Aliment> aliments = getAliments();
+        outputPort.displayAliments(
+                new GetAlimentsResponseModel(
+                        aliments.stream()
+                                .map(AlimentResponse::from)
+                                .toList()
+                )
+        );
+
+    }
+
+    private List<Aliment> getAliments() {
+        return alimentRepository.findAll();
+    }
+}
---
+++ src/main/java/io/github/oliviercap/chefduplacard/application/getaliments/ports/IGetAlimentsOutputPort.java
@@ -0,0 +1,5 @@
+package io.github.oliviercap.chefduplacard.application.getaliments.ports;
+
+public interface IGetAlimentsOutputPort {
+    void displayAliments(GetAlimentsResponseModel responseModel);
+}
