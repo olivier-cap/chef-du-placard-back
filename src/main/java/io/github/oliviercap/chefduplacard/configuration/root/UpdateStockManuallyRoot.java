0e7b487f179075c6a8aaadcc6ab7cce9078665de
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-05-27 10:49:10 +0200

    Udpate stock manually root

---
+++ src/main/java/io/github/oliviercap/chefduplacard/configuration/root/UpdateStockManuallyRoot.java
@@ -0,0 +1,25 @@
+package io.github.oliviercap.chefduplacard.configuration.root;
+
+import io.github.oliviercap.chefduplacard.adapters.persistence.jpa.repository.aliment.AlimentRepository;
+import io.github.oliviercap.chefduplacard.adapters.persistence.jpa.repository.stock.StockRepository;
+import io.github.oliviercap.chefduplacard.adapters.persistence.jpa.repository.unit.UnitRepository;
+import io.github.oliviercap.chefduplacard.application.updatestockmanually.UpdateStockManuallyUseCase;
+import io.github.oliviercap.chefduplacard.application.updatestockmanually.port.IUpdateStockManuallyOutputPort;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UpdateStockManuallyRoot {
+
+    public UpdateStockManuallyUseCase UpdateStockManuallyRoot(StockRepository stockRepository,
+                                                              AlimentRepository alimentRepository,
+                                                              UnitRepository unitRepository,
+                                                              IUpdateStockManuallyOutputPort iUpdateStockManuallyOutputPort)
+    {
+        return new UpdateStockManuallyUseCase(
+                stockRepository,
+                alimentRepository,
+                unitRepository,
+                iUpdateStockManuallyOutputPort
+        );
+    }
+}
