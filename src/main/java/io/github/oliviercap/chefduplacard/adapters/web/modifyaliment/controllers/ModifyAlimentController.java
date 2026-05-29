940063e6ab1f917823b109fdbde438db01030eff
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-05-29 23:15:10 +0200

    modify aliment basic controller

--- src/main/java/io/github/oliviercap/chefduplacard/adapters/web/modifyaliment/controllers/ModifyAlimentController.java
+++ src/main/java/io/github/oliviercap/chefduplacard/adapters/web/modifyaliment/controllers/ModifyAlimentController.java
@@ -1,4 +1,26 @@
 package io.github.oliviercap.chefduplacard.adapters.web.modifyaliment.controllers;
 
+import io.github.oliviercap.chefduplacard.adapters.web.modifyaliment.ModifyAlimentViewModel;
+import io.github.oliviercap.chefduplacard.application.modifyaliment.ModifyAlimentRequestModel;
+import io.github.oliviercap.chefduplacard.application.modifyaliment.ports.IModifyAlimentInputPort;
+import io.github.oliviercap.chefduplacard.application.modifyaliment.ports.IModifyAlimentOutputPort;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ModifyAlimentController {
+
+    IModifyAlimentInputPort inputPort;
+    IModifyAlimentOutputPort outputPort;
+
+    @PostMapping("/modfiyAliment")
+    public ModifyAlimentViewModel modifyAliment(
+        @RequestParam String alimentName,
+        @RequestParam String alimentDescription
+    ){
+        inputPort.execute(new ModifyAlimentRequestModel(alimentName, alimentDescription));
+
+        return outputPort.getViewModel();
+    }
 }
