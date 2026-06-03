8f632fcc0c3df77689310fe0b7b3b2bf6acb32b7
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-06-03 19:29:48 +0200

    Add from metho in menuresponse and menuline response

--- src/main/java/io/github/oliviercap/chefduplacard/application/dto/MenuResponse.java
+++ src/main/java/io/github/oliviercap/chefduplacard/application/dto/MenuResponse.java
@@ -1,9 +1,19 @@
 package io.github.oliviercap.chefduplacard.application.dto;
 
+import io.github.oliviercap.chefduplacard.domain.menu.Menu;
+
 import java.util.List;
 
 public record MenuResponse(
         String menuName,
         List<MenuLineResponse> menuLineResponseList
 ) {
+    public static MenuResponse from(Menu menu){
+        return new MenuResponse(
+                menu.getName(),
+                menu.getMenuLines().stream()
+                        .map(MenuLineResponse::from)
+                        .toList()
+        );
+    }
 }
