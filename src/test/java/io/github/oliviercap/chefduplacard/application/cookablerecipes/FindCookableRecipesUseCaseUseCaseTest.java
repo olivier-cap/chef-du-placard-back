7a87ce8057661aa33f3e6988674167913e8726c5
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-06-04 12:22:08 +0200

    Update tests because of add of a new method in recipe repository

--- src/test/java/io/github/oliviercap/chefduplacard/application/cookablerecipes/FindCookableRecipesUseCaseUseCaseTest.java
+++ src/test/java/io/github/oliviercap/chefduplacard/application/cookablerecipes/FindCookableRecipesUseCaseUseCaseTest.java
@@ -48,6 +48,11 @@ class FakeRecipeRepository implements IRecipeRepository {
     public Optional<RecipeJpa> findJpaById(Long id) {
         return Optional.empty();
     }
+
+    @Override
+    public Optional<RecipeJpa> findJpaByName(String recipeName) {
+        return Optional.empty();
+    }
 }
 
 class FakeStockRepository implements IStockRepository {
