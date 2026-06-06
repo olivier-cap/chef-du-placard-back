3815b29dbc7babfbb20dcf015fece86e4959d621
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-06-06 10:17:26 +0200

    adapt GetMenuRoot to import MenuViewQuery instead of MenuRepository

--- src/main/java/io/github/oliviercap/chefduplacard/configuration/root/GetMenuRoot.java
+++ src/main/java/io/github/oliviercap/chefduplacard/configuration/root/GetMenuRoot.java
@@ -3,6 +3,7 @@ package io.github.oliviercap.chefduplacard.configuration.root;
 import io.github.oliviercap.chefduplacard.adapters.persistence.jpa.repository.menu.MenuRepository;
 import io.github.oliviercap.chefduplacard.application.getmenu.GetMenuUseCase;
 import io.github.oliviercap.chefduplacard.application.getmenu.ports.IGetMenuOutputPort;
+import io.github.oliviercap.chefduplacard.application.ports.query.IMenuViewQuery;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,11 @@ public class GetMenuRoot {
 
     @Bean
     public GetMenuUseCase GetMenuRoot(
-            MenuRepository menuRepository,
+            IMenuViewQuery menuViewQuery,
             IGetMenuOutputPort outputPort
     ) {
         return new GetMenuUseCase(
-                menuRepository,
+                menuViewQuery,
                 outputPort
         );
     }
