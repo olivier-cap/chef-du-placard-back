ec86e4ab10c5d2405252196f408e15295811d8e1
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-08-16 14:08:23 +0200

    cookable menu controller and request for controller

--- src/main/java/io/github/oliviercap/chefduplacard/adapters/web/savenewmenu/controllers/SaveNewMenuRequest.java
+++ src/main/java/io/github/oliviercap/chefduplacard/adapters/web/savenewmenu/controllers/SaveNewMenuRequest.java
@@ -3,7 +3,7 @@ package io.github.oliviercap.chefduplacard.adapters.web.savenewmenu;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record UpdateStockRequest(
+public record SaveNewMenuRequest(
         String menuName,
         List<MenuLine> menuLines
 ) {
