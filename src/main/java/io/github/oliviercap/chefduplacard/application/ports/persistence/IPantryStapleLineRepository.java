a3e8d733afef3c318bdf9a044663f05184514266
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-09-10 18:15:21 +0200

    pantry staples line repository

---
+++ src/main/java/io/github/oliviercap/chefduplacard/application/ports/persistence/IPantryStapleLineRepository.java
@@ -0,0 +1,9 @@
+package io.github.oliviercap.chefduplacard.application.ports.persistence;
+
+import io.github.oliviercap.chefduplacard.domain.pantry_staples.PantryStaplesLine;
+
+import java.math.BigDecimal;
+
+public interface IPantryStapleLineRepository {
+    PantryStaplesLine modifyPantryStaplesLine(Long pantryStaplesLineId, BigDecimal quantity, Long unitId);
+}
