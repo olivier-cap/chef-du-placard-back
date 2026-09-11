11cb63138e1d2ecea35b45877ec5a2a23d9ca929
Olivier <189439718+olivier-cap@users.noreply.github.com>
2026-09-12 01:37:20 +0200

    usecase cookable menus with recipes proposal stock not ok

--- src/main/java/io/github/oliviercap/chefduplacard/application/menu/cookablemenus/CookableMenusUseCase.java
+++ src/main/java/io/github/oliviercap/chefduplacard/application/menu/cookablemenus/CookableMenusUseCase.java
@@ -45,12 +45,15 @@
         public void execute(CookableMenusRequestModel cookableMenusRequestModel) {
             Objects.requireNonNull(cookableMenusRequestModel, "cookableMenusRequestModel must not be null");
 
-            List<Recipe> menu = findCookableMenus(
+            CookableMenus response = findCookableMenus(
                     cookableMenusRequestModel.stockId(),
                     cookableMenusRequestModel.nbMealToPrepare(),
                     cookableMenusRequestModel.nbPeople(),
                     cookableMenusRequestModel.recipeFilters()
             );
+
+            List<Recipe> menu = response.recipesStockOk();
+            List<Recipe> proposal = response.recipesNoStock();
 
             String message;
             if (menu.size() == cookableMenusRequestModel.nbMealToPrepare()) {
@@ -66,11 +69,80 @@
                     menu.stream()
                             .map(RecipeResponse::from)
                             .toList(),
+                    proposal.stream().map(RecipeResponse::from).toList(),
                     message
             );
 
             cookableMenusOutputPort.displayCookableMenus(responseModel);
         }
+
+
+//        private void findCookableMenusWithDate(CookableMenusRequestModelDate requestModelDate) {
+//            //On traite un par un des ensembles <Jour(date), repas1(?), repas2(?), repas3(?), nbPersonnes>, triés par date
+//            //On stocke dans une liste les ids des recettes déjà proposées.
+//            //Tant qu'une autre recette est disponible, on en prend une différente.
+//            //Si pas d'autre disponible, on recherche parmi toutes les recettes.
+//            //Sinon, prendre la 1er recette parmi les non disponibles et dire "aliment(s) manquant(s)
+//
+//            Long stockId = requestModelDate.stockId();
+//
+//            Stock stock = stockRepository.findById(stockId).orElseThrow(() -> new DomainException("Stock not found"));
+//            VirtualStockFactory virtualStockFactory = new VirtualStockFactory();
+//            VirtualStock virtualStock = virtualStockFactory.createForMenuPreparation(stock);//Utilisation du virtualstock exclusivement ici
+//
+//
+//            //tri de la liste des repas demandés par date croissante
+//            List<CookableMenusRequestModelDate.menuPerDayRequest> menuPerDayRequestList = requestModelDate.menuPerDayRequestList();
+//            menuPerDayRequestList.sort(Comparator.comparing(CookableMenusRequestModelDate.menuPerDayRequest::date));
+//
+//            List<Recipe> alreadyDone = new ArrayList<>();
+//
+//            List<Recipe> recipes = recipeRepository.findAll();
+//            boolean recipeCandidatesExist = !recipes.isEmpty();
+//
+//            List<Recipe> breakfasts = recipes.stream().filter(r -> r.getType().getName().equalsIgnoreCase("breakfast")).toList();
+//            List<Recipe> lunchs = recipes.stream().filter(r -> r.getType().getName().equalsIgnoreCase("lunch")).toList();
+//            List<Recipe> diner = recipes.stream().filter(r -> r.getType().getName().equalsIgnoreCase("diner")).toList();
+//
+//            List<MenuLine> menuLines = new ArrayList<>();
+//
+//            //recherche des repas demandés.
+//            //si aucun repas possible avec le stock, proposition recette hors stock.
+//            //enregistre pour chaque recette si stock/pas stock pour affichage utilisateur
+//            if(recipeCandidatesExist) {
+//                for (CookableMenusRequestModelDate.menuPerDayRequest request : menuPerDayRequestList) {
+//                    if(request.breakfast() && !breakfasts.isEmpty()) {
+//                        searchRecipe(breakfasts, alreadyDone, virtualStock, request.nbPeople());
+//                        menuLines.add(
+//                                new MenuLine()
+//                        )
+//                    }
+//
+//                }
+//            }
+//
+//        }
+//
+//        private Recipe searchRecipe(List<Recipe> candidates, List<Recipe> alreadyDone, VirtualStock virtualStock, Integer nbPeople) {
+//            //toutes les recettes de ce type ont déjà été proposées, on propose la 1er de la liste
+//            if (alreadyDone.containsAll(candidates)) {
+//                return candidates.getFirst();
+//            }
+//
+//            //recherche d'une recette faisable avec le stock
+//            for(Recipe candidate : candidates) {
+//                List<Ingredient> requiredIngredients = candidate.computeRequiredIngredients(nbPeople);
+//                if(virtualStock.covers(requiredIngredients).covered() && !alreadyDone.contains(candidate)){
+//                    virtualStock.consume(requiredIngredients);
+//                    return candidate;
+//                }
+//            }
+//
+//            //Pas de recettes faisables avec le stock, on propose la 1er recette non faisable avec le stock et non faite
+//            return candidates.stream().filter(c -> !alreadyDone.contains(c)).findFirst()
+//                    .orElseThrow(() -> new DomainException("impossible to select a recipe"));
+//        }
+
 
         /**
          * Recherche des recettes réalisables pour nbPeople personnes, en fonction des filtres choisis.
@@ -80,8 +152,12 @@
          * @param nbPeople
          * @return
          */
-        private List<Recipe> findCookableMenus(Long stockId, int nbMealToPrepare, int nbPeople, List<RecipeFilter> recipeFilters) {
-            List<Recipe> menusRecipes = new ArrayList<>(); //liste des recettes du menu
+        //private List<Recipe> findCookableMenus(Long stockId, int nbMealToPrepare, int nbPeople, List<RecipeFilter> recipeFilters) {
+         private CookableMenus findCookableMenus(Long stockId, int nbMealToPrepare, int nbPeople, List<RecipeFilter> recipeFilters) {
+
+                List<Recipe> menusRecipes = new ArrayList<>(); //liste des recettes du menu
+            List<Recipe> menusRecipesNoStock = new ArrayList<>(); //liste des recettes du menu
+
 
             Stock stock = stockRepository.findById(stockId).orElseThrow(() -> new DomainException("Stock not found"));
             VirtualStockFactory virtualStockFactory = new VirtualStockFactory();
@@ -112,7 +188,7 @@
                 recipeCandidatesExist = !filteredList.isEmpty();
 
                 if(recipeCandidatesExist){
-                    //selection de la "meilleure" recette selon critères auto/critères utilisateur (?)
+                    //selection de la "meilleure" recette
                     //Par défaut 1ère recette de la liste
                     Recipe selectedRecipe = recipeRankingService.selectBestRecipe(filteredList);
 
@@ -125,6 +201,20 @@
                     menusRecipes.add(selectedRecipe);
                 }
             }
-            return menusRecipes;
+            if(menusRecipes.size() < nbMealToPrepare && !recipes.isEmpty()) {
+                int nbMealRestant = nbMealToPrepare - menusRecipes.size();
+                for(int i=0; i < nbMealRestant - 1; i++) {
+                    if(recipes.size() > i) {
+                        menusRecipesNoStock.add(recipes.get(i));
+                    } else {
+                        menusRecipesNoStock.add(recipes.getFirst());
+                    }
+                }
+            }
+
+            CookableMenus result = new CookableMenus(menusRecipes, menusRecipesNoStock);
+            return result;
+
+            //return menusRecipes;
         }
     }
